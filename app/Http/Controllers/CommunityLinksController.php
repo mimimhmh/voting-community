a7a3d4e9404147723758adeb393d442e3e014d15
Yang Wang <mimimhmh@users.noreply.github.com>
2017-06-02 23:11:03 +1200

    Contribute a Link - 1

--- app/Http/Controllers/CommunityLinksController.php
+++ app/Http/Controllers/CommunityLinksController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers;
 
 
 use App\CommunityLink;
+use App\Http\Requests\LinksRequest;
 
 class CommunityLinksController extends Controller
 {
-    //
+    public function __construct() {
+
+        $this->middleware('auth')->except(['index']);
+    }
+
     public function index() {
 
         $links = CommunityLink::paginate(25);
         return view('community.index', compact('links'));
+    }
+
+    public function store(LinksRequest $request) {
+
+        CommunityLink::from(auth()->user())->contribute($request->all());
+
+        return back();
     }
 }
--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -2,24 +2,54 @@
 
 @section('content')
     <div class="container">
-        <h1> Community </h1>
-        <ul class="Links">
-            @foreach($links as $link)
-                <li class="Links__link">
-                    <a href="{{ $link->link }}" target="_blank">
-                        {{ $link->title }}
-                    </a>
+        <div class="row">
+            <div class="col-md-8">
+                <h1> Community </h1>
+                <ul class="Links">
+                    @foreach($links as $link)
+                        <li class="Links__link">
+                            <a href="{{ $link->link }}" target="_blank">
+                                {{ $link->title }}
+                            </a>
 
-                    <small>
-                        contributed by:
-                        <a href="#">
-                        {{ $link->creator->name }}
-                        </a>
-                        {{ $link->updated_at->diffForHumans() }}
-                    </small>
-                </li>
+                            <small>
+                                contributed by:
+                                <a href="#">
+                                    {{ $link->creator->name }}
+                                </a>
+                                {{ $link->updated_at->diffForHumans() }}
+                            </small>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
 
-            @endforeach
-        </ul>
+            <div class="col-md-4">
+                <h3>Contribute a Link</h3>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form method="post" action="/community">
+
+                            {{ csrf_field() }}
+
+                            <div class="form-group">
+                                <label for="title">Title: </label>
+                                <input type="text" class="form-control" id="title" name="title"
+                                       placeholder="Enter title">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="link">Link: </label>
+                                <input class="form-control" id="link" name="link" placeholder="Your URL">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Contribute Link</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
